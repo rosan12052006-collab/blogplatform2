95c970343715399ee0df263684636c21b1b2140f
Rosans <rosan12052006@gmail.com>
2026-06-30 17:38:14 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,6 @@
-import axios from 'axios';
-
-const api = axios.create({ baseURL: '/api' });
-
-
+const api = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api` : '/api' 
+});
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
